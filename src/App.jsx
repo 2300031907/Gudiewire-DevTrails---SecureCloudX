1075a4741632af921fc5eeabd53a78a755ef6d1e
vk-vaishnavi10 <kavanoorvaishnavi09@gmail.com>
2026-04-04 00:26:54 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { GLOBAL_CSS } from './styles';
+
 import Screen1Landing from './Screen1Landing';
 import { Screen2Mobile, Screen3OTP, Screen4Profile, Screen5Platforms, Screen6Bank } from './OnboardingSteps';
 import Dashboard from './Dashboard';
